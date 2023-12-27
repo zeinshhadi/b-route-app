a64d142d05dcca0c240fba274aaab7a7ac4b82b6
zeinshhadi <zeinshhadi@hotmail.com>
2023-12-27 18:51:08 +0200

    added jwt

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -42,4 +42,18 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+        public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
