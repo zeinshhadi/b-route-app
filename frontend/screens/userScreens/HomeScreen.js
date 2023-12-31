97ace4056a4717d6fba35868e530b15471c69b80
zeinshhadi <zeinshhadi@hotmail.com>
2023-12-31 21:30:52 +0200

    get user location + location permission

--- frontend/screens/userScreens/HomeScreen.js
+++ frontend/screens/userScreens/HomeScreen.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { Alert, StyleSheet, View, Button } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import MapView from "react-native-maps";
+import { getCurrentPositionAsync, useForegroundPermissions, PermissionStatus } from "expo-location";
 
 const HomeScreen = () => {
   const [token, setToken] = useState(null);
-
+  const [locationPermissionInformation, requestPermission] = useForegroundPermissions();
   useEffect(() => {
     const fetchToken = async () => {
       const retrievedToken = await retrieveToken();
@@ -24,15 +25,38 @@ const HomeScreen = () => {
       return null;
     }
   };
-
+  async function verifyPermissions() {
+    if (locationPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
+    }
+    if (locationPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert("Insufficient Permissions!", "You need to grant location permissions to use this app.");
+      return false;
+    }
+    return true;
+  }
+  async function getLocationHandler() {
+    const hasPermission = await verifyPermissions();
+    if (!hasPermission) {
+      return;
+    }
+    const location = await getCurrentPositionAsync();
+    console.log(location);
+  }
   return (
     <View style={styles.container}>
       <MapView style={styles.map} />
+
+      <View style={styles.buttonContainer}>
+        <Button title="Get Location" onPress={() => getLocationHandler()} />
+      </View>
     </View>
   );
 };
 
 export default HomeScreen;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -40,5 +64,10 @@ const styles = StyleSheet.create({
   map: {
     width: "100%",
     height: "100%",
+  },
+  buttonContainer: {
+    position: "absolute",
+    bottom: 16,
+    alignSelf: "center",
   },
 });
