f4839c44f8306dc757ea3528b2146c94f2379759
zeinshhadi <zeinshhadi@hotmail.com>
2023-12-31 21:08:49 +0200

    added map preview for user home screen

--- frontend/screens/userScreens/HomeScreen.js
+++ frontend/screens/userScreens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Text } from "react-native";
+import { StyleSheet, View } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import MapView from "react-native-maps";
 
 const HomeScreen = () => {
   const [token, setToken] = useState(null);
@@ -24,7 +25,20 @@ const HomeScreen = () => {
     }
   };
 
-  return <Text>Hello from Home Screen</Text>;
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.map} />
+    </View>
+  );
 };
 
 export default HomeScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
