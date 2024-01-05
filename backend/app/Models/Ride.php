55571ae26b070f41cdc669b8ccfedf2b97605da7
zeinshhadi <zeinshhadi@hotmail.com>
2024-01-05 11:37:28 +0200

    Backend <> added relations in Ride Model

--- backend/app/Models/Ride.php
+++ backend/app/Models/Ride.php
@@ -9,4 +9,14 @@ class Ride extends Model
 {
     use HasFactory;
   protected  $fillable =['start_location','end_location','rate','price','user_id','driver_id'];
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+
+    public function driver()
+    {
+        return $this->belongsTo(Driver::class, 'driver_id');
+    }
 }
