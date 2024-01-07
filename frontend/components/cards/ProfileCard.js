73a5cdfca40761e5a43b25ae5103a5fb3199456d
zeinshhadi <zeinshhadi@hotmail.com>
2024-01-07 23:15:32 +0200

    FrontEnd <> Added screen to start building the edit profile page

--- frontend/components/cards/ProfileCard.js
+++ frontend/components/cards/ProfileCard.js
@@ -1,17 +1,20 @@
+import Icon from "react-native-vector-icons/FontAwesome";
 import React from "react";
 import { View, StyleSheet, Text, Pressable } from "react-native";
 
-const ProfileCard = () => {
+const ProfileCard = ({ cardTitle, cardDetail }) => {
   return (
-    <View style={styles.profileCardContainer}>
-      <Pressable>
+    <Pressable>
+      <View style={styles.profileCardContainer}>
         <View style={styles.leftCardProfile}>
           <Text>Your Email : </Text>
           <Text>Zeinshhadi@gmail.com</Text>
         </View>
-        <View></View>
-      </Pressable>
-    </View>
+        <View>
+          <Icon name="arrow-right" size={30} color="black" />
+        </View>
+      </View>
+    </Pressable>
   );
 };
 
@@ -25,6 +28,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#D9D9D9",
     borderRadius: 10,
     margin: 30,
+    justifyContent: "space-around",
+    alignItems: "center",
   },
   leftCardProfile: {
     justifyContent: "space-around",
---
+++ frontend/screens/userScreens/EditProfile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ProfileCard from "../../components/cards/ProfileCard";
+
+const EditProfile = () => {
+  return (
+    <>
+      <ProfileCard cardTitle="Phone Number " cardDetail="zeinshhadi@gmail.com" />
+    </>
+  );
+};
+
+export default EditProfile;
