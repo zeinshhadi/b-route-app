0f029cc15e9f3d64b861a1f87f0df5c32984af08
zeinshhadi <zeinshhadi@hotmail.com>
2024-01-08 23:50:07 +0200

    Frontend <> Added style for the review card component

--- frontend/components/cards/ReviewCard.js
+++ frontend/components/cards/ReviewCard.js
@@ -1,8 +1,37 @@
 import React from "react";
+import { StyleSheet } from "react-native";
+import { Text } from "react-native";
 import { View } from "react-native";
 
-const ReviewCard = () => {
-  return <View></View>;
+const ReviewCard = ({ cardTitle, cardDetail, reviewText }) => {
+  return (
+    <View style={styles.reviewCardContainer}>
+      <View style={styles.reviewCard}>
+        <Text>{cardTitle}</Text>
+        <Text>{cardDetail}</Text>
+      </View>
+      <View style={styles.reviewCard}>
+        <Text>{reviewText}</Text>
+      </View>
+    </View>
+  );
 };
 
 export default ReviewCard;
+
+const styles = StyleSheet.create({
+  reviewCardContainer: {
+    flexDirection: "row",
+    height: 80,
+    backgroundColor: "#D9D9D9",
+    borderRadius: 10,
+    marginTop: 5,
+    justifyContent: "space-around",
+    alignItems: "center",
+    width: "100%",
+  },
+  reviewCard: {
+    height: "100%",
+    justifyContent: "space-around",
+  },
+});
