091aa6c5a2a751ef8d3d19b16304233840fad828
zeinshhadi <zeinshhadi@hotmail.com>
2024-01-08 23:57:31 +0200

    Frontend <> Added styling to review screen

--- frontend/screens/adminScreens/ReviewScreen.js
+++ frontend/screens/adminScreens/ReviewScreen.js
@@ -1,14 +1,30 @@
 import React from "react";
 import ReviewCard from "../../components/cards/ReviewCard";
+import { StyleSheet, View } from "react-native";
 
 const ReviewScreen = () => {
   return (
-    <ReviewCard
-      cardTitle={"Zein Shhadi"}
-      cardDetail={"*" * 3}
-      reviewText={"What A Ride ! Just what we needed to arrive on time !"}
-    />
+    <View style={styles.reviewContainer}>
+      <View style={styles.reviewInnerContainer}>
+        <ReviewCard
+          cardTitle={"Zein Shhadi"}
+          cardDetail={`${"****"} `}
+          reviewText={"What A Ride ! Just what we needed to arrive on time !"}
+        />
+      </View>
+    </View>
   );
 };
 
 export default ReviewScreen;
+
+const styles = StyleSheet.create({
+  reviewContainer: {
+    justifyContent: "center",
+    width: "100%",
+    alignItems: "center",
+  },
+  reviewInnerContainer: {
+    width: "90%",
+  },
+});
