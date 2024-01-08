8a98c9bbbd622785bd6e50af4fd235914bb0147a
zeinshhadi <zeinshhadi@hotmail.com>
2024-01-08 16:43:13 +0200

    Frontend <> Added auth reducer logic

--- frontend/core/redux/reducers/authReducer.js
+++ frontend/core/redux/reducers/authReducer.js
@@ -4,3 +4,26 @@ const initialState = {
   token: null,
   error: null,
 };
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "LogIn Success":
+      return {
+        ...state,
+        isLoggedIn: true,
+        user: action.payload.user,
+        token: action.payload.token,
+        error: null,
+      };
+    case "LogIn Failed":
+      return {
+        ...state,
+        isLoggedIn: false,
+        user: null,
+        token: null,
+        error: action.payload,
+      };
+  }
+};
+
+export default authReducer;
