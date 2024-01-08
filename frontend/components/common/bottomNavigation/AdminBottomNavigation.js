d5f109625976e8d4834b70c4883af84ed43927a4
zeinshhadi <zeinshhadi@hotmail.com>
2024-01-09 01:38:22 +0200

    Frontend <> Added bottom navigation for admin

---
+++ frontend/components/common/bottomNavigation/AdminBottomNavigation.js
@@ -0,0 +1,24 @@
+// AdminBottomTabNavigator.js
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import AdminHomeScreen from "../../../screens/adminScreens/AdminHomeScreen";
+import EditProfile from "../../../screens/userScreens/EditProfile";
+import ChatScreen from "../../../screens/common/ChatScreen";
+import ReviewScreen from "../../../screens/adminScreens/ReviewScreen";
+import HomeScreen from "../../../screens/userScreens/HomeScreen";
+
+const Tab = createBottomTabNavigator();
+
+const AdminBottomNavigation = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={AdminHomeScreen} />
+      <Tab.Screen name="Chat" component={ChatScreen} />
+      <Tab.Screen name="Rides" component={ReviewScreen} />
+      <Tab.Screen name="EditProfile" component={EditProfile} />
+    </Tab.Navigator>
+  );
+};
+
+export default AdminBottomNavigation;
