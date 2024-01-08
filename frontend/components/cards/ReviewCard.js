701da1e1d4c458e941e44005a0d85ebc4a4fb382
zeinshhadi <zeinshhadi@hotmail.com>
2024-01-09 00:07:24 +0200

    Frontend <> Fixed Styling for review card

--- frontend/components/cards/ReviewCard.js
+++ frontend/components/cards/ReviewCard.js
@@ -7,10 +7,10 @@ const ReviewCard = ({ cardTitle, cardDetail, reviewText }) => {
   return (
     <View style={styles.reviewCardContainer}>
       <View style={styles.reviewCard}>
-        <Text>{cardTitle}</Text>
-        <Text>{cardDetail}</Text>
+        <Text style={styles.reviewCardText}>{cardTitle}</Text>
+        <Text style={styles.reviewCardText}>{cardDetail}</Text>
       </View>
-      <View style={styles.reviewCard}>
+      <View style={styles.reviewCardDetail}>
         <Text>{reviewText}</Text>
       </View>
     </View>
@@ -21,17 +21,26 @@ export default ReviewCard;
 
 const styles = StyleSheet.create({
   reviewCardContainer: {
-    flexDirection: "row",
-    height: 80,
+    height: 100,
     backgroundColor: "#D9D9D9",
     borderRadius: 10,
     marginTop: 5,
-    justifyContent: "space-around",
+    justifyContent: "flex-start",
     alignItems: "center",
     width: "100%",
+    padding: 10,
+    gap: 20,
   },
   reviewCard: {
-    height: "100%",
-    justifyContent: "space-around",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+  reviewCardText: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  reviewCardDetail: {
+    width: "100%",
   },
 });
