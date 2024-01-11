6dceb34bf2a92aace8a0f194139420c2aafb4cfb
zeinshhadi <zeinshhadi@hotmail.com>
2024-01-11 04:46:46 +0200

    Backend <> Added get all zones api

--- backend/app/Http/Controllers/ZonesController.php
+++ backend/app/Http/Controllers/ZonesController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Zone;
 use Illuminate\Http\Request;
 
 class ZonesController extends Controller
 {
-    //
+  public function all_zones(){
+    $zone=Zone::all();
+    return response()->json(['Zones are as follows : '=>$zone]);
+  }
 }
