569b91503f3d9504fab4ba8534f71ac5da758edb
zeinshhadi <zeinshhadi@hotmail.com>
2024-01-11 03:40:05 +0200

    Backend <> Fixed the model

--- backend/app/Models/Bus.php
+++ backend/app/Models/Bus.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Bus extends Model
 {
     use HasFactory;
-    protected $fillable = ['vin','color','plate_number','model','number_of_seats'];
+    protected $fillable = ['vin','color','plate_number','model','number_of_seats','zone_id'];
 public function driver()
 {
     return $this->hasOne(Driver::class);
