94e14a119cce45f67a96f9724df849c517e77750
zeinshhadi <zeinshhadi@hotmail.com>
2024-01-13 23:51:25 +0200

    Frontend <> Added options for bottom tab navigator

--- frontend/components/common/bottomNavigation/DriverBottomNavigation.js
+++ frontend/components/common/bottomNavigation/DriverBottomNavigation.js
@@ -4,7 +4,10 @@ import Colors from "../../../utils/colors";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 const Tab = createBottomTabNavigator();
 const DriverBottomNavigation = () => {
-  return <Tab.Navigator></Tab.Navigator>;
+  return (
+    <Tab.Navigator
+      screenOptions={{ tabBarActiveTintColor: Colors.primary500, tabBarInactiveTintColor: "gray" }}></Tab.Navigator>
+  );
 };
 
 export default DriverBottomNavigation;
