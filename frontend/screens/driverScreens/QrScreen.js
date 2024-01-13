4fc32581fbd6586dd3851e36052586da11755e7e
zeinshhadi <zeinshhadi@hotmail.com>
2024-01-13 22:39:41 +0200

    Frontend <> Initialized qr screen component

---
+++ frontend/screens/driverScreens/QrScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text } from "react-native";
+import { View } from "react-native";
+
+const QrScreen = () => {
+  return (
+    <View>
+      <Text>Driver qr</Text>
+    </View>
+  );
+};
+
+export default QrScreen;
