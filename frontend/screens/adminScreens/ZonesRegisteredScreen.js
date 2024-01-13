9e396396810965d7cd22d0a944153c1d170d6ee2
zeinshhadi <zeinshhadi@hotmail.com>
2024-01-13 03:00:47 +0200

    Frontend <> Added Fetch and flatlist for zones

--- frontend/screens/adminScreens/ZonesRegisteredScreen.js
+++ frontend/screens/adminScreens/ZonesRegisteredScreen.js
@@ -1,15 +1,36 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { FlatList, View } from "react-native";
 import DetailsCard from "../../components/cards/DetailsCard";
 import SearchBar from "../../components/common/SearchBar";
 import { StyleSheet } from "react-native";
+import { useSelector } from "react-redux";
+import axios from "axios";
 
 const ZonesRegisteredScreen = () => {
+  const [zones, setZones] = useState;
+  const authState = useSelector((state) => state.auth);
+  const authorization = "bearer" + authState.token;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://192.168.0.101:8000/api/zones", {
+          headers: { Authorization: authorization },
+        });
+        setZones(response.data);
+      } catch (error) {
+        console.log("Error Fetching " + error);
+      }
+    };
+  }, []);
+  const renderItem = () => {
+    <DetailsCard cardTitle={"Zone#"} cardDetail={"zoneName"} tempText={"MoreDetails"} status={"status"} />;
+  };
   return (
     <View style={styles.BusesRegisteredContainer}>
       <View style={styles.innerContainer}>
         <SearchBar />
-        <DetailsCard cardTitle={"Zone#"} cardDetail={"zoneName"} tempText={"MoreDetails"} status={"status"} />
+
+        <FlatList data={zones} renderItem={renderItem} />
       </View>
     </View>
   );
