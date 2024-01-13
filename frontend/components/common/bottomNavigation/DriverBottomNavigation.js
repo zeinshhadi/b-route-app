5f0ce32a8c9a3b7ba0e7c2dda5b1f8839ac32ecf
zeinshhadi <zeinshhadi@hotmail.com>
2024-01-13 23:52:12 +0200

    Frontend <> Added first tab for driver tabs

--- frontend/components/common/bottomNavigation/DriverBottomNavigation.js
+++ frontend/components/common/bottomNavigation/DriverBottomNavigation.js
@@ -2,11 +2,20 @@ import React from "react";
 import { Ionicons } from "@expo/vector-icons";
 import Colors from "../../../utils/colors";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import QrScreen from "../../../screens/driverScreens/QrScreen";
 const Tab = createBottomTabNavigator();
 const DriverBottomNavigation = () => {
   return (
-    <Tab.Navigator
-      screenOptions={{ tabBarActiveTintColor: Colors.primary500, tabBarInactiveTintColor: "gray" }}></Tab.Navigator>
+    <Tab.Navigator screenOptions={{ tabBarActiveTintColor: Colors.primary500, tabBarInactiveTintColor: "gray" }}>
+      <Tab.Screen
+        name="Home Screen"
+        component={QrScreen}
+        options={{
+          headerShown: true,
+          tabBarIcon: ({ color, size }) => <Ionicons name="home" color={color} size={size} />,
+        }}
+      />
+    </Tab.Navigator>
   );
 };
 
