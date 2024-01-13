3c45ad3eb30c0e85a96ff35fb63992065fdddb81
zeinshhadi <zeinshhadi@hotmail.com>
2024-01-13 22:56:28 +0200

    Frontend <> Added permission for location

--- frontend/screens/driverScreens/QrScreen.js
+++ frontend/screens/driverScreens/QrScreen.js
@@ -7,6 +7,18 @@ const QrScreen = () => {
   driver_id = authState.user.id;
   const dataToEncode = "YourDataHere";
 
+  async function verifyPermissions() {
+    if (locationPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
+    }
+    if (locationPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert("Insufficient Permissions!", "You need to grant location permissions to use this app.");
+      return false;
+    }
+    return true;
+  }
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <QRCode value={dataToEncode} size={200} color="black" backgroundColor="white" />
