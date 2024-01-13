84a0a3fede468f16e98924b6875fddd87eb0ded4
zeinshhadi <zeinshhadi@hotmail.com>
2024-01-13 23:49:59 +0200

    Frontend <> Added imports for driver bottom tabs

--- frontend/components/common/bottomNavigation/DriverBottomNavigation.js
+++ frontend/components/common/bottomNavigation/DriverBottomNavigation.js
@@ -1,5 +1,8 @@
 import React from "react";
-
+import { Ionicons } from "@expo/vector-icons";
+import Colors from "../../../utils/colors";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+const Tab = createBottomTabNavigator();
 const DriverBottomNavigation = () => {
   return <div>DriverBottomNavigation</div>;
 };
