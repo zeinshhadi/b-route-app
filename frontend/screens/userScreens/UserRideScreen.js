6797a0ed2e20db09fba3628fce336c8fe3eddaf2
zeinshhadi <zeinshhadi@hotmail.com>
2024-01-14 02:54:28 +0200

    Frontend <> Initialized User ride screen

---
+++ frontend/screens/userScreens/UserRideScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const UserRideScreen = () => {
+  return (
+    <View>
+      <Text>Rides Screen</Text>
+    </View>
+  );
+};
+
+export default UserRideScreen;
