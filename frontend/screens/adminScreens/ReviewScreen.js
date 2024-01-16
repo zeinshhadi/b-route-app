6fcee8decf4c07a048330c260c712b1fddf2f990
zeinshhadi <zeinshhadi@hotmail.com>
2024-01-16 03:03:24 +0200

    Frontend <> Created a flatlist in review screen

--- frontend/screens/adminScreens/ReviewScreen.js
+++ frontend/screens/adminScreens/ReviewScreen.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import ReviewCard from "../../components/cards/ReviewCard";
-import { StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 import SearchBar from "../../components/common/SearchBar";
 import { Ionicons } from "@expo/vector-icons";
 import axios from "axios";
 import { useSelector } from "react-redux";
 const ReviewScreen = () => {
   const authState = useSelector((state) => state.auth);
-  const authorization = "bearer " + auth.token;
+  const authorization = "bearer " + authState.token;
   const [data, setData] = useState();
   useEffect(() => {
     const fetchData = async () => {
@@ -15,30 +15,35 @@ const ReviewScreen = () => {
         const response = await axios.get("http://192.168.0.101:8000/api/feedback", {
           headers: { Authorization: authorization },
         });
-        console.log(response);
+        setData(response.data.reviews);
       } catch (error) {
         console.log("error", error);
       }
     };
     fetchData();
   }, []);
-
+  const renderItem = () => {
+    return (
+      <ReviewCard
+        cardTitle={"Zein Shhadi"}
+        cardDetail={
+          <View style={styles.starContainer}>
+            <Ionicons name="star" />
+            <Ionicons name="star" />
+            <Ionicons name="star" />
+            <Ionicons name="star" />
+          </View>
+        }
+        reviewText={"What A Ride ! Just what we needed to arrive on time !"}
+      />
+    );
+  };
   return (
     <View style={styles.reviewContainer}>
       <View style={styles.reviewInnerContainer}>
         <SearchBar />
-        <ReviewCard
-          cardTitle={"Zein Shhadi"}
-          cardDetail={
-            <View style={styles.starContainer}>
-              <Ionicons name="star" />
-              <Ionicons name="star" />
-              <Ionicons name="star" />
-              <Ionicons name="star" />
-            </View>
-          }
-          reviewText={"What A Ride ! Just what we needed to arrive on time !"}
-        />
+
+        <FlatList data={data} renderItem={renderItem} keyExtractor={(item) => item.id} />
       </View>
     </View>
   );
