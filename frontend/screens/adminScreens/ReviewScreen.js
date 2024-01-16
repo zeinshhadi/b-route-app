ec66a7717e5bd9ada2ed78a7d9e2312e406cbe05
zeinshhadi <zeinshhadi@hotmail.com>
2024-01-16 02:52:44 +0200

    Frontend <> Added auth token to headers

--- frontend/screens/adminScreens/ReviewScreen.js
+++ frontend/screens/adminScreens/ReviewScreen.js
@@ -4,12 +4,17 @@ import { StyleSheet, View } from "react-native";
 import SearchBar from "../../components/common/SearchBar";
 import { Ionicons } from "@expo/vector-icons";
 import axios from "axios";
+import { useSelector } from "react-redux";
 const ReviewScreen = () => {
+  const authState = useSelector((state) => state.auth);
+  const authorization = "bearer " + auth.token;
   const [data, setData] = useState();
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://192.168.0.101:8000/api/feedback");
+        const response = await axios.get("http://192.168.0.101:8000/api/feedback", {
+          headers: { Authorization: authorization },
+        });
         console.log(response);
       } catch (error) {
         console.log("error", error);
