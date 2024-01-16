4054e6a20cbd173a5106714f4c47b18924f276b4
zeinshhadi <zeinshhadi@hotmail.com>
2024-01-16 02:23:02 +0200

    Backend <> Added api route of get free bus

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -47,6 +47,7 @@ Route::controller(BusesController::class)->group(function () {
     Route::get('all/buses', 'getAllBuses');
     Route::get('/bus', 'getAllBusesAndDriver');
     Route::get('/bus/zone/{id}','getBusesByZone');
+    Route::get('/free/bus','getFreeBuses');
 
 });
 Route::controller(DriversController::class)->group(function () {
