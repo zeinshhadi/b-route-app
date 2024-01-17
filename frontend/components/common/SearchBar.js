acb38fe212fdc9b78da20a4ad8224fabdd048b68
zeinshhadi <zeinshhadi@hotmail.com>
2024-01-18 00:17:34 +0200

    Frontend <> Fixed search bar and added icon

--- frontend/components/common/SearchBar.js
+++ frontend/components/common/SearchBar.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { Text, TextInput, View, StyleSheet } from "react-native";
 import Colors from "../../utils/colors";
-
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 const SearchBar = () => {
   const [searchText, setSearchText] = useState();
   return (
     <View style={styles.searchContainer}>
+      <Ionicons style={styles.inputSearchIcon} name="search" size={15} color={Colors.primary600} />
       <TextInput
         style={styles.inputSearchDesign}
         placeholder="Search"
@@ -20,17 +21,22 @@ const SearchBar = () => {
 export default SearchBar;
 const styles = StyleSheet.create({
   searchContainer: {
+    flexDirection: "row",
     padding: 10,
     borderRadius: 20,
     borderWidth: 2,
     borderColor: Colors.primary500,
     height: 40,
     marginVertical: 10,
-    justifyContent: "center",
+    justifyContent: "flex-start",
     alignItems: "center",
     width: "100%",
   },
   inputSearchDesign: {
+    alignSelf: "flex-start",
+    marginLeft: 10,
+  },
+  inputSearchIcon: {
     alignSelf: "flex-start",
     marginLeft: 10,
   },
