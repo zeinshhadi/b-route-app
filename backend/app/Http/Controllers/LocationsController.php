908d40a7ca3a8eb8e5a44d529ae7f363409a2c56
zeinshhadi <zeinshhadi@hotmail.com>
2024-01-20 22:46:52 +0200

    Backend <> Fixed driver location api to accept one request of this driver_is

--- backend/app/Http/Controllers/LocationsController.php
+++ backend/app/Http/Controllers/LocationsController.php
@@ -14,18 +14,24 @@ class LocationsController extends Controller
         $user =Auth::user();
         $driver_id = Driver::where('user_id', $user->id)->first();
         $driver_id= $driver_id->id;
-        if (!$user) {
+        $location_found = Location::where('driver_id', $driver_id)->first();
+        if(!$location_found){
+                    if (!$user) {
             return response()->json(['no driver',$driver_id]);
         }
         $location = Location::create([
             'driver_id'=>$driver_id,
-            'longitude'=>$request->longitude,
-            'latitude'=>$request->latitude
+            'longitude'=>$request->lon,
+            'latitude'=>$request->lat
         ]);
         if (!$location) {
            return response()->json(['fail']);
         }
-            return response()->json(['driver',$driver_id]);
+            return response()->json(['location',$location]);
+        }else{
+             return response()->json(['fail',$location_found]);
+        }
+
         }
 
     public function update_driver_location(Request $request){
