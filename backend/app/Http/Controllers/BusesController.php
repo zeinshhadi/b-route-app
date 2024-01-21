7f34a24def9331306169b2359fb71447c227e6b0
zeinshhadi <zeinshhadi@hotmail.com>
2024-01-21 04:10:58 +0200

    Backend <> Added get bus and driver

--- backend/app/Http/Controllers/BusesController.php
+++ backend/app/Http/Controllers/BusesController.php
@@ -10,27 +10,58 @@ use Illuminate\Support\Facades\Auth;
 class BusesController extends Controller
 {
 
-    public function create_bus(Request $request){
+public function create_bus(Request $request){
              $bus= Bus::create($request->all());
              return response()->json(['status'=>'success'],200);
 
     }
-        public function getAllBuses()
+public function getAllBuses()
     {
         $buses = Bus::all();
         return response()->json(['buses' => $buses], 200);
     }
+
+
         public function getAllBusesAndDriver()
     {
         $buses = Bus::has('driver')->with(['driver.user'])->get();
         return response()->json(['buses' => $buses], 200);
     }
-    public function getBusesByZone($zone_id){
-        $bus = Bus::where('zone_id',$zone_id)->has('driver')->with(['driver.user'])->get();
-         return response()->json(['bus' => $bus], 200);
+
+public function getBusAndDriver(Request $request)
+
+
+    {   
+        $driver_id = $request->driver_id;
+
+
+        $buses = Bus::has('driver')->with(['driver.user'])->where('id',$driver_id)->get();
+
+
+        return response()->json(['buses' => $buses], 200);
     }
+
+
+
+public function getBusesByZone($zone_id){
+
+
+            $bus = Bus::where('zone_id',$zone_id)->has('driver')->with(['driver.user'])->get();
+
+
+             return response()->json(['bus' => $bus], 200);
+        }
+
+
 public function getFreeBuses(){
-    $buses = Bus::doesntHave('driver')->get();
-    return response()->json(['buses'=>$buses]);
-}
-}
+
+
+        $buses = Bus::doesntHave('driver')->get();
+
+
+        return response()->json(['buses'=>$buses]);
+
+
+    }
+
+    }
