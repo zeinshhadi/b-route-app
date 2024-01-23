82dbe70e0614341cd19280a881e74e7b75dd967b
zeinshhadi <zeinshhadi@hotmail.com>
2024-01-24 01:43:17 +0200

    Frontend <> Fixed ripple effect

--- frontend/components/grid/GridComponent.js
+++ frontend/components/grid/GridComponent.js
@@ -4,14 +4,16 @@ import Colors from "../../utils/colors";
 
 const GridComponent = ({ children, onPress }) => {
   return (
-    <Pressable
-      onPress={onPress}
-      android_ripple={{ color: Colors.primary500 }}
-      style={({ pressed }) => [styles.button, pressed ? styles.buttonPressed : null]}>
-      <View style={styles.cardGridContainer}>
-        <Text style={styles.containerTitle}>{children}</Text>
-      </View>
-    </Pressable>
+    <View style={styles.buttonView}>
+      <Pressable
+        onPress={onPress}
+        android_ripple={{ color: Colors.primary500 }}
+        style={({ pressed }) => [styles.button, pressed ? styles.buttonPressed : null]}>
+        <View style={styles.cardGridContainer}>
+          <Text style={styles.containerTitle}>{children}</Text>
+        </View>
+      </Pressable>
+    </View>
   );
 };
 
@@ -28,11 +30,16 @@ const styles = StyleSheet.create({
     shadowColor: Colors.primary500,
     shadowOpacity: 4,
     shadowRadius: 10,
+    overflow: "hidden",
   },
   containerTitle: {
     textAlign: "center",
   },
   buttonPressed: {
     opacity: 0.5,
+  },
+  buttonView: {
+    borderRadius: 10,
+    overflow: "hidden",
   },
 });
