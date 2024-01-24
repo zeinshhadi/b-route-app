d2aea54939b74a0eed13fe817047ba8ce74bd8da
zeinshhadi <zeinshhadi@hotmail.com>
2024-01-24 18:06:13 +0200

    Backend <> Created a driver seed

--- backend/database/seeders/BusSeeder.php
+++ backend/database/seeders/BusSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Bus;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,8 +11,26 @@ class BusSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+ public function run()
     {
-        //
+        $zones = [1, 2, 3, 4, 5];
+        $models = ['Mazda', 'Toyota', 'Mercedes', 'Ford', 'Chevrolet', 'Honda'];
+
+        for ($i = 1; $i <= 6; $i++) {
+            Bus::create([
+                'vin' => rand(1000000000000000, 9999999999999999),
+                'color' => $this->getRandomColor(),
+                'plate_number' => rand(100000, 999999),
+                'model' => $models[array_rand($models)],
+                'number_of_seats' => rand(30, 50),
+                'zone_id' => $zones[array_rand($zones)],
+            ]);
+        }
+    }
+
+    private function getRandomColor()
+    {
+        $colors = ['Red', 'Blue', 'Green', 'Yellow', 'Black', 'White', 'Silver', 'Gray'];
+        return $colors[array_rand($colors)];
     }
 }
--- backend/database/seeders/DriverSeeder.php
+++ backend/database/seeders/DriverSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Bus;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,8 +12,30 @@ class DriverSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        
+        $drivers = User::where('role_type', 'driver')->get();
+
+        
+        foreach ($drivers as $driver) {
+            
+            $bus = Bus::inRandomOrder()->first();
+
+            
+            $driverLicense = mt_rand(10000000, 99999999);
+
+            
+            $driverLicenseString = (string) $driverLicense;
+
+            
+            $driverLicenseTruncated = substr($driverLicenseString, 0, 255);
+
+            
+            $driver->driver()->create([
+                'bus_id' => $bus->id,
+                'driver_license' => $driverLicenseTruncated,
+            ]);
+        }
     }
 }
