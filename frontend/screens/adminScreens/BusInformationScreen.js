27c3c53fc2130eb9ae57f9021c2d08b17645f553
zeinshhadi <zeinshhadi@hotmail.com>
2024-01-26 18:44:26 +0200

    Frontend <> Fixed Bus information screen"

--- frontend/screens/adminScreens/BusInformationScreen.js
+++ frontend/screens/adminScreens/BusInformationScreen.js
@@ -1,23 +1,44 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Pressable, Text, Alert } from "react-native";
+import { StyleSheet, View, Pressable, Text, ActivityIndicator } from "react-native";
 import { useRoute } from "@react-navigation/native";
 import DriverDetailsCard from "../../components/cards/DriverDetailsCard";
 import BusInfoCard from "../../components/cards/busDetails/BusInfoCard";
 import { MaterialIcons } from "@expo/vector-icons";
 import Colors from "../../utils/colors";
+import axios from "axios";
+import { Url } from "../../core/helper/Url";
+import { useSelector } from "react-redux";
 const BusInformationScreen = ({ navigation }) => {
+  const authState = useSelector((state) => state.auth);
+  const authorization = "bearer " + authState.token;
+  const [loading, setLoading] = useState(false);
   const route = useRoute();
 
   const item = route.params.item;
-  const { id, model, plate_number, color, vin, number_of_seats, zone_id } = item;
-
+  const { id, model, plate_number, color, vin, number_of_seats } = item;
+  const userId = item.driver.user.id;
+  console.log(userId);
   const [showDelete, setShowDelete] = useState(false);
 
-  const handleDelete = () => {};
+  const handleDelete = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.delete(`${Url}/api/delete/driver/${userId}`, {
+        headers: { Authorization: authorization },
+      });
+      if (response.status === "success") {
+        navigation.navigate("BusesRegisteredScreen");
+      }
+    } catch (error) {
+      setLoading(false);
+      console.log(`error : ${error}`);
+    }
+    setLoading(false);
+  };
   const driverName = item.driver.user.first_name + " " + item.driver.user.last_name;
   return (
     <View style={styles.BusInformationContainer}>
-      <DriverDetailsCard driverFirstName={item.driver.user.first_name} driverLastName={item.driver.user.last_name} />
+      <DriverDetailsCard />
       <BusInfoCard
         id={id}
         model={model}
@@ -36,7 +57,11 @@ const BusInformationScreen = ({ navigation }) => {
 
       {showDelete && (
         <Pressable style={styles.deleteButton} onPress={handleDelete}>
-          <Text style={styles.deleteButtonText}>Confirm Delete</Text>
+          {loading ? (
+            <ActivityIndicator size={"small"} color={"white"} />
+          ) : (
+            <Text style={styles.deleteButtonText}>Confirm Delete</Text>
+          )}
         </Pressable>
       )}
     </View>
