567f5ee4eceea4fb825a6e6d3f01fe8efbb0f5f1
zeinshhadi <zeinshhadi@hotmail.com>
2024-01-27 20:48:39 +0200

    Backend <> Fixed Driver Seeder

--- backend/database/seeders/DriverSeeder.php
+++ backend/database/seeders/DriverSeeder.php
@@ -2,36 +2,40 @@
 
 namespace Database\Seeders;
 
-use App\Models\Bus;
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Bus;
 
 class DriverSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     *
+     * @return void
      */
     public function run()
     {
-        
         $drivers = User::where('role_type', 'driver')->get();
 
-        
+        $assignedBusIds = [];
+
         foreach ($drivers as $driver) {
-            
-            $bus = Bus::inRandomOrder()->first();
 
-            
+            $bus = Bus::whereNotIn('id', $assignedBusIds)->inRandomOrder()->first();
+
+
+            if (!$bus) {
+                break;
+            }
+
+
+            $assignedBusIds[] = $bus->id;
+
+     
             $driverLicense = mt_rand(10000000, 99999999);
+            $driverLicenseTruncated = substr((string) $driverLicense, 0, 255);
 
-            
-            $driverLicenseString = (string) $driverLicense;
-
-            
-            $driverLicenseTruncated = substr($driverLicenseString, 0, 255);
-
-            
+  
             $driver->driver()->create([
                 'bus_id' => $bus->id,
                 'driver_license' => $driverLicenseTruncated,
@@ -39,3 +43,4 @@ class DriverSeeder extends Seeder
         }
     }
 }
+
