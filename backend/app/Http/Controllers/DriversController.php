58142f23e5d0c1fc0d023e52e3be1df303928b07
zeinshhadi <zeinshhadi@hotmail.com>
2024-01-27 13:53:48 +0200

    Backend <> Fixed create driver to handle image

--- backend/app/Http/Controllers/DriversController.php
+++ backend/app/Http/Controllers/DriversController.php
@@ -5,58 +5,13 @@ namespace App\Http\Controllers;
 use App\Models\Driver;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 class DriversController extends Controller
 {
-
-// public function create_driver(Request $request)
-// {
-//     $validator = Validator::make($request->all(), [
-//         'first_name' => 'required|string|max:255',
-//         'last_name' => 'required|string|max:255',
-//         'email' => 'required|email|unique:users',
-//         'password' => 'required|min:6',
-//         'phone_number' => 'required|string|max:15',
-//         'driver_license' => 'required|string|max:255',
-//         'bus_id' => 'required|exists:buses,id',
-      
-//     ]);
-
-//     if ($validator->fails()) {
-//         Log::error($validator->errors());
-//         return response()->json(['errors' => $validator->errors()], 422);
-//     }
-
-//     $user = User::create([
-//         'first_name' => $request->input('first_name'),
-//         'last_name' => $request->input('last_name'),
-//         'email' => $request->input('email'),
-//         'password' => bcrypt($request->input('password')),
-//         'phone_number' => $request->input('phone_number'),
-//         'role_type' => 'driver', 
-//     ]);
-
- 
-// Extract the image file name from the provided URI
-// $imageName = $user->id . '_' . time() . '_' . pathinfo($request->input('image'), PATHINFO_BASENAME);
-
-// $driver = Driver::create([
-//     'image' => $request->image,
-//     'driver_license' => $request->input('driver_license'),
-//     'user_id' => $user->id,
-//     'bus_id' => $request->input('bus_id'),
-// ]);
-
-// Store the image using the provided URI
-// $imageContent = file_get_contents($request->input('image'));
-// Storage::put('public/' . $imageName, $imageContent);
-
-
-//     return response()->json(['status' => 'success'], 201);
-// }
 
 public function create_driver(Request $request)
 {
@@ -68,7 +23,7 @@ public function create_driver(Request $request)
         'phone_number' => 'required|string|max:15',
         'driver_license' => 'required|string|max:255',
         'bus_id' => 'required|exists:buses,id',
-      
+        'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
     ]);
 
     if ($validator->fails()) {
@@ -85,20 +40,20 @@ public function create_driver(Request $request)
         'role_type' => 'driver', 
     ]);
 
+ 
+    $image = $request->file('image');
+    $imageName = $user->id . '_' . time() . '_' . $image->getClientOriginalName();
+    Storage::putFileAs('public/driverImages', $image, $imageName);
 
     $driver = Driver::create([
-        'image' => $request->image,
+        'image' => $imageName,
         'driver_license' => $request->input('driver_license'),
         'user_id' => $user->id,
         'bus_id' => $request->input('bus_id'),
     ]);
-    
-    return response()->json(['status' => 'success', 'message' => 'Driver created successfully']);
+
+    return response()->json(['status' => 'success'], 201);
 }
-
-
-
-
 
 
      public function deleteDriverByUserId($userId)
