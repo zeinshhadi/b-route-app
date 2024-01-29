4912de57cc772de9de296f670e3fc13897607514
zeinshhadi <zeinshhadi@hotmail.com>
2024-01-29 04:47:25 +0200

    Backend <> Added test to start and end ride

--- backend/tests/Feature/EndRideTest.php
+++ backend/tests/Feature/EndRideTest.php
@@ -6,19 +6,24 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class StartRideTest extends TestCase
+class EndRideTest extends TestCase
 {
- 
-    public function test_create_ride_api(): void
-  {
-        $driverResponse = $this->postJson('/api/login',[
+    /**
+     * A basic feature test example.
+     */
+public function test_end_ride_api(): void
+{
+
+    $driverResponse = $this->postJson('/api/login', [
         'email' => 'albert@gmail.com',
         'password' => '123123',
     ]);
     $driverResponse->assertStatus(200);
 
- 
+
     $driverUserId = $driverResponse->json()['user']['id'];
+
+
     $passengerResponse = $this->postJson('/api/login', [
         'email' => 'john@gmail.com',
         'password' => '123123',
@@ -27,16 +32,29 @@ class StartRideTest extends TestCase
     $passengerToken = $passengerResponse['authorization']['token'];
 
 
-
     $response = $this->withHeaders([
         'Authorization' => 'Bearer ' . $passengerToken,
     ])->postJson('/api/add/ride', [
         'start_longitude' => 32.0,
         'start_latitude' => 30.0,
-        'user_id' => $driverUserId, 
+        'user_id' => $driverUserId,
     ]);
 
     $response->assertStatus(200)
-        ->assertJson(['response from create ride ']); 
+        ->assertJson(['response from create ride ']);
+
+    $endResponse = $this->withHeaders([
+        'Authorization' => 'Bearer ' . $passengerToken,
+    ])->postJson('/api/end/ride', [
+        'end_longitude' => 32.1,
+        'end_latitude' => 30.1,
+        'user_id' => $driverUserId,
+    ]);
+
+
+    $endResponse->assertStatus(200)
+        ->assertJson(['status' => 'success']);
+
 }
+
 }
